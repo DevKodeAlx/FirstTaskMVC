a6dbec993fc30c259c8ae1590402f0173d7f4576
Aleksander <aleksander.sandberg89@gmail.com>
2025-03-31 17:52:54 +0200

    Finished result.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using FirstTaskMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstTaskMVC.Controllers
+{    
+    public class MovieController : Controller
+    {
+        private static List<MovieViewModel> _movies = new List<MovieViewModel>();
+        public IActionResult Index()
+        {
+            //MovieViewModel Movie = new MovieViewModel()
+            //{ MovieTitle = "The Godfather", Director = "Francis Ford Coppola", Year = 1972 };
+            //return View(Movie);
+            return View(_movies);
+        }
+
+        public IActionResult Create()
+        {
+            var movieVm = new MovieViewModel();
+            return View(movieVm);
+        }
+        public IActionResult CreateMovie(MovieViewModel movieViewModel)
+        {
+            _movies.Add(movieViewModel);
+            return RedirectToAction(nameof(Index));
+            // returns view("Index");
+        
+        }
+    }
+}
---
+++ Models/MovieViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstTaskMVC.Models
+{
+    public class MovieViewModel
+    {
+        public string MovieTitle { get; set; }
+        public string Director { get; set; }
+
+        public int Year { get; set; }
+    }
+}
